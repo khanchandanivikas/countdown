1b8adefee820b370258f326de78e67e0aebf499f
vikas <khanchandani58@gmail.com>
2022-12-07 22:34:49 +0000

    date updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import {
+  useState,
+  useEffect
+} from "react";
 
 function App() {
   const [timerDays, setTimerDays] = useState("00");
@@ -10,7 +13,7 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       const today = new Date();
-      const future = new Date("June 1, 2022 00:00:00");
+      const future = new Date("June 1, 2023 00:00:00");
       const diff = Date.parse(future) - Date.parse(today);
       let days = Math.floor(diff / (1000 * 60 * 60 * 24));
       let hour = Math.floor(diff / (1000 * 60 * 60));
@@ -27,33 +30,51 @@ function App() {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-  return (
-    <div className="App">
-      <h1 className="encabezado">LANZAMOS PRONTO</h1>
-      <div className="times">
-        <div>
-        <h1 className="time">{timerDays}</h1>
-        <h1 className="content">Dias</h1>
-        </div>
-        <div>
-        <h1 className="time">{timerHours}</h1>
-        <h1 className="content">Horas</h1>
-        </div>
-        <div>
-        <h1 className="time">{timerMins}</h1>
-        <h1 className="content">Mins</h1>
-        </div>
-        <div>
-        <h1 className="time">{timerSecs}</h1>
-        <h1 className="content">Secs</h1>
-        </div>
-      </div>
-      <div className="social">
-        <a href="/"><i className="fab fa-facebook-square"></i></a>
-        <a href="/"><i className="fab fa-pinterest"></i></a>
-        <a href="/"><i className="fab fa-instagram"></i></a>
-      </div>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    h1 className = "encabezado" > LANZAMOS PRONTO < /h1> <
+    div className = "times" >
+    <
+    div >
+    <
+    h1 className = "time" > {
+      timerDays
+    } < /h1> <
+    h1 className = "content" > Dias < /h1> <
+    /div> <
+    div >
+    <
+    h1 className = "time" > {
+      timerHours
+    } < /h1> <
+    h1 className = "content" > Horas < /h1> <
+    /div> <
+    div >
+    <
+    h1 className = "time" > {
+      timerMins
+    } < /h1> <
+    h1 className = "content" > Mins < /h1> <
+    /div> <
+    div >
+    <
+    h1 className = "time" > {
+      timerSecs
+    } < /h1> <
+    h1 className = "content" > Secs < /h1> <
+    /div> <
+    /div> <
+    div className = "social" >
+    <
+    a href = "/" > < i className = "fab fa-facebook-square" > < /i></a >
+    <
+    a href = "/" > < i className = "fab fa-pinterest" > < /i></a >
+    <
+    a href = "/" > < i className = "fab fa-instagram" > < /i></a >
+    <
+    /div> <
+    /div>
   );
 }
 
